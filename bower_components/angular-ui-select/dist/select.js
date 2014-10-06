22adf0be37987f517d0f7f1222b0c097b6966845
Cheng Zhu <cheng@weteachme.com>
2014-10-06 18:10:27 +1100

    update ui-select

--- bower_components/angular-ui-select/dist/select.js
+++ bower_components/angular-ui-select/dist/select.js
@@ -1,7 +1,7 @@
 /*!
  * ui-select
  * http://github.com/angular-ui/ui-select
- * Version: 0.7.0 - 2014-10-06T03:53:35.349Z
+ * Version: 0.7.0 - 2014-10-06T07:03:32.705Z
  * License: MIT
  */
 
@@ -582,20 +582,34 @@
 
         $select.multiple = angular.isDefined(attrs.multiple);
 
-        var defaultSortableOptions = {
-           placeholder: "ui-state-highlight",
-           helper: "clone",
-           opacity: 0.5,
-           tolerance: "pointer"
-        };
-        $select.sortableOptions = defaultSortableOptions;
-        
-
         $select.onSelectCallback = $parse(attrs.onSelect);
 
         if ($select.multiple) {
           $select.onRemoveCallback = $parse(attrs.onRemove);
         }
+
+        var defaultSortableOptions = {
+            placeholder: "ui-state-highlight",
+            opacity: 0.7,
+            stop: function() {
+              angular.forEach(ngModel.$modelValue, function(item){
+                $select.onRemoveCallback(scope, {
+                    $item: item
+                });
+              });
+
+              angular.forEach(ngModel.$modelValue, function(item){
+                var locals = {};
+                locals[$select.parserResult.itemName] = item;
+                $select.onSelectCallback(scope, {
+                    $item: item,
+                    $model: $select.parserResult.modelMapper(scope, locals)
+                });
+              });
+            }
+           
+        };
+        $select.sortableOptions = defaultSortableOptions;
 
         //From view --> model
         ngModel.$parsers.unshift(function (inputValue) {
@@ -951,7 +965,7 @@
 }());
 
 angular.module("ui.select").run(["$templateCache", function($templateCache) {$templateCache.put("bootstrap/choices.tpl.html","<ul class=\"ui-select-choices ui-select-choices-content dropdown-menu\" role=\"menu\" aria-labelledby=\"dLabel\" ng-show=\"$select.items.length > 0\"><li class=\"ui-select-choices-group\"><div class=\"divider\" ng-show=\"$select.isGrouped && $index > 0\"></div><div ng-show=\"$select.isGrouped\" class=\"ui-select-choices-group-label dropdown-header\">{{$group.name}}</div><div class=\"ui-select-choices-row\" ng-class=\"{active: $select.isActive(this), disabled: $select.isDisabled(this)}\"><a href=\"javascript:void(0)\" class=\"ui-select-choices-row-inner\"></a></div></li></ul>");
-$templateCache.put("bootstrap/match-multiple.tpl.html","<span class=\"ui-select-match\" ng-model=\"$select.selected\" ui-sortable=\"{{$select.sortableOptions}}\"><span ng-repeat=\"$item in $select.selected\"><a style=\"margin-right: 3px;\" class=\"ui-select-match-item btn btn-default btn-xs btn-flat\" tabindex=\"-1\" type=\"button\" ng-disabled=\"$select.disabled\" ng-click=\"$select.activeMatchIndex = $index;\" ng-class=\"{\'btn-primary\':$select.activeMatchIndex === $index}\"><span class=\"close ui-select-match-close\" ng-hide=\"$select.disabled\" ng-click=\"$select.removeChoice($index)\">&nbsp;&times;</span> <span uis-transclude-append=\"\"></span></a></span></span>");
+$templateCache.put("bootstrap/match-multiple.tpl.html","<span class=\"ui-select-match\" ng-model=\"$select.selected\" ui-sortable=\"$select.sortableOptions\"><span ng-repeat=\"$item in $select.selected\"><a style=\"margin-right: 3px;\" class=\"ui-select-match-item btn btn-default btn-xs btn-flat\" tabindex=\"-1\" type=\"button\" ng-disabled=\"$select.disabled\" ng-click=\"$select.activeMatchIndex = $index;\" ng-class=\"{\'btn-primary\':$select.activeMatchIndex === $index}\"><span class=\"close ui-select-match-close\" ng-hide=\"$select.disabled\" ng-click=\"$select.removeChoice($index)\">&nbsp;&times;</span> <span uis-transclude-append=\"\"></span></a></span></span>");
 $templateCache.put("bootstrap/match.tpl.html","<button type=\"button\" class=\"btn btn-default btn-flat form-control ui-select-match\" tabindex=\"-1\" ng-hide=\"$select.open\" ng-disabled=\"$select.disabled\" ng-class=\"{\'btn-default-focus\':$select.focus}\" ;=\"\" ng-click=\"$select.activate()\"><span ng-show=\"$select.searchEnabled && $select.isEmpty()\" class=\"text-muted\">{{$select.placeholder}}</span> <span ng-hide=\"$select.isEmpty()\" ng-transclude=\"\"></span> <span class=\"caret ui-select-toggle\" ng-click=\"$select.toggle($event)\"></span></button>");
 $templateCache.put("bootstrap/select-multiple.tpl.html","<div class=\"ui-select-multiple ui-select-bootstrap dropdown form-control\" ng-class=\"{open: $select.open}\"><div><div class=\"ui-select-match\"></div><input type=\"text\" autocomplete=\"off\" autocorrect=\"off\" autocapitalize=\"off\" spellcheck=\"false\" class=\"ui-select-search input-xs\" placeholder=\"{{$select.getPlaceholder()}}\" ng-disabled=\"$select.disabled\" ng-hide=\"$select.disabled\" ng-click=\"$select.activate()\" ng-model=\"$select.search\"></div><div class=\"ui-select-choices\"></div></div>");
 $templateCache.put("bootstrap/select.tpl.html","<div class=\"ui-select-bootstrap dropdown\" ng-class=\"{open: $select.open}\"><div class=\"ui-select-match\"></div><input type=\"text\" autocomplete=\"off\" tabindex=\"-1\" class=\"form-control ui-select-search\" placeholder=\"{{$select.placeholder}}\" ng-model=\"$select.search\" ng-show=\"$select.searchEnabled && $select.open\"><div class=\"ui-select-choices\"></div></div>");
